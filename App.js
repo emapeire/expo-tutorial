5577bd929a4523a263e6a0bf434a02caedf1210e
Emanuel Peire <emapeire@outlook.com>
2023-02-06 16:08:21 -0300

    use an image picker

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
 import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -14,7 +17,7 @@ export default function App() {
     });
 
     if (!result.canceled) {
-      console.log(result);
+      setSelectedImage(result.assets[0].uri);
     } else {
       alert('You need to select an image.');
     }
@@ -23,7 +26,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,8 +1,12 @@
 import { StyleSheet, Image } from 'react-native';
 
-export default function ImageViewer({ placeholderImageSource }) {
+export default function ImageViewer({ placeholderImageSource, selectedImage }) {
+  const imageSource = selectedImage !== null
+    ? { uri: selectedImage }
+    : placeholderImageSource;
+
   return (
-    <Image source={placeholderImageSource} style={styles.image} />
+    <Image source={imageSource} style={styles.image} />
   );
 }
 
